438a89e3c3bce5b0f864d2fecbe1e21588c7e19b
mastronelli88@gmail.com <mastronelli88@gmail.com>
2024-06-19 07:59:47 -0300

    build

--- .dev/src/js/index.js
+++ .dev/src/js/index.js
@@ -1,18 +1,41 @@
 $(".slider-slick").slick({
-	slidesToShow: 1,
-	mobileFirst: true,
-	slidesToScroll: 1,
-	infinite: false,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	prevArrow: $(".prev"),
-	nextArrow: $(".next"),
-	responsive: {
-		breakpoint: 1024,
-		settings: {
-			slidesToShow: 5,
-			slidesToScroll: 1,
-			autoplay: false,
-		},
-	},
+  slidesToShow: 1,
+  mobileFirst: true,
+  slidesToScroll: 1,
+  infinite: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow: $(".prev"),
+  nextArrow: $(".next"),
+  responsive: {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: false,
+    },
+  },
+})
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const data = new FormData(form)
+  const redirectUrl = window.location.origin + "/typ.html"
+
+  fetch("https://www.rdstation.com.br/api/1.3/conversions", {
+    method: "POST",
+    body: data,
+  })
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = redirectUrl
+      } else {
+        console.error("Erro ao enviar o formulário:", response.statusText)
+      }
+    })
+    .catch((error) => {
+      console.error("Erro na solicitação:", error)
+    })
 })
